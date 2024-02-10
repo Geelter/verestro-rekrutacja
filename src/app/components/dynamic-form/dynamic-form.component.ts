b46e20eae46283ad40804f6e79f2a86d6c1f5401
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2024-02-10 10:49:35 +0100

    Add form data as required input on the dynamic form component

--- src/app/components/dynamic-form/dynamic-form.component.ts
+++ src/app/components/dynamic-form/dynamic-form.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {JsonFormData} from "../../models/json-form-data.interface";
 
 @Component({
   selector: 'app-dynamic-form',
   standalone: true,
   imports: [],
   templateUrl: './dynamic-form.component.html',
-  styleUrl: './dynamic-form.component.scss'
+  styleUrl: './dynamic-form.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DynamicFormComponent {
-
+  @Input({required: true}) jsonFormData!: JsonFormData;
 }
