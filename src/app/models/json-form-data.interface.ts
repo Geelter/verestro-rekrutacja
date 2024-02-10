45342602a34645e235d18a817d395555c2a9b498
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2024-02-10 10:47:46 +0100

    Create interfaces representing survey configuration

---
+++ src/app/models/json-form-data.interface.ts
@@ -0,0 +1,28 @@
+interface JsonFormValidators {
+  min?: number;
+  max?: number;
+  required?: boolean;
+  requiredTrue?: boolean;
+  email?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  pattern?: string;
+  nullValidator?: boolean;
+}
+interface JsonFormControlOptions {
+  min?: string;
+  max?: string;
+  step?: string;
+  icon?: string;
+}
+interface JsonFormControl {
+  name: string;
+  label: string;
+  value: string;
+  type: string;
+  options?: JsonFormControlOptions;
+  validators: JsonFormValidators;
+}
+export interface JsonFormData {
+  controls: JsonFormControl[];
+}
