12b2dbc0240c783311542fe75cbc754d1a96bc33
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2024-02-10 14:02:18 +0100

    Define Input/Output bindings for dynamic form component. Create base FormGroup instance.

--- src/app/components/dynamic-form/dynamic-form.component.ts
+++ src/app/components/dynamic-form/dynamic-form.component.ts
@@ -1,14 +1,29 @@
-import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, inject, Input, OnInit, Output} from '@angular/core';
 import {JsonFormData} from "../../models/json-form-data.interface";
+import {AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-dynamic-form',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule],
   templateUrl: './dynamic-form.component.html',
   styleUrl: './dynamic-form.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DynamicFormComponent {
+export class DynamicFormComponent implements OnInit {
   @Input({required: true}) jsonFormData!: JsonFormData;
+  @Output() formSubmitted = new EventEmitter<AbstractControl>();
+
+  private readonly formBuilder = inject(FormBuilder);
+  dynamicForm: FormGroup = this.formBuilder.group({});
+
+  onSubmit() {
+    this.formSubmitted.emit(this.dynamicForm.value);
+  }
+
+  ngOnInit() {
+    console.log(this.jsonFormData);
+    console.log(this.dynamicForm);
+  }
 }
