9175429f2e68b02ee631fa768ca97928b856a43a
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2024-02-10 20:57:23 +0100

    Generate files for form editor

---
+++ src/app/components/form-editor/form-editor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-editor',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-editor.component.html',
+  styleUrl: './form-editor.component.scss'
+})
+export class FormEditorComponent {
+
+}
