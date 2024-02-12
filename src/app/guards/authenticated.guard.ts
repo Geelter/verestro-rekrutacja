e9dbf40dbd1daa47ec6117ebc8af7827f638aa44
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2024-02-12 19:14:24 +0100

    Create route guards for authentication state

---
+++ src/app/guards/authenticated.guard.ts
@@ -0,0 +1,11 @@
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from "@angular/core";
+import {SupabaseAuthService} from "../services/supabase/supabase-auth.service";
+
+export const authenticatedGuard: CanActivateFn = async () => {
+  const router = inject(Router);
+  const authService = inject(SupabaseAuthService);
+
+  const session = await authService.getSession();
+  return session ? true : router.parseUrl('/auth');
+};
