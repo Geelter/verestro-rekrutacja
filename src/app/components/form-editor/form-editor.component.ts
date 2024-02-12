d25e0aea266e0abdd03a8c553abdec6e123225ce
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2024-02-12 14:51:48 +0100

    Create methods used for instantiating the default question formGroup and parsing existing questions

--- src/app/components/form-editor/form-editor.component.ts
+++ src/app/components/form-editor/form-editor.component.ts
@@ -34,4 +34,102 @@ export class FormEditorComponent {
     title: new FormControl('New Survey', {nonNullable: true, validators: [Validators.required]}),
     questions: new FormArray<FormGroup>([], [Validators.required, Validators.minLength(1)])
   });
+
+  private createQuestion(jsonSurveyQuestion: JsonSurveyQuestion | null = null): FormGroup<QuestionGroup> {
+    const newQuestion = new FormGroup<QuestionGroup>({
+      questionLabel: new FormControl(
+        jsonSurveyQuestion?.questionLabel ?? '',
+        {nonNullable: true, validators: [Validators.required]}
+      ),
+      questionControlName: new FormControl(
+        jsonSurveyQuestion?.questionControlName ?? '',
+        {nonNullable: true, validators: [Validators.required]}
+      ),
+      questionType: new FormControl(
+        jsonSurveyQuestion?.questionType ?? 'text',
+        {nonNullable: true, validators: [Validators.required]}
+      ),
+      questionValidators: new FormGroup<QuestionValidatorsGroup>({
+        min: new FormControl(
+          jsonSurveyQuestion?.questionValidators.min ?? null
+        ),
+        max: new FormControl(
+          jsonSurveyQuestion?.questionValidators.max ?? null
+        ),
+        minLength: new FormControl(
+          jsonSurveyQuestion?.questionValidators.minLength ?? null
+        ),
+        maxLength: new FormControl(
+          jsonSurveyQuestion?.questionValidators.maxLength ?? null
+        ),
+        required: new FormControl(
+          jsonSurveyQuestion?.questionValidators.required ?? false,
+          {nonNullable: true}
+        ),
+        requiredTrue: new FormControl(
+          jsonSurveyQuestion?.questionValidators.requiredTrue ?? false,
+          {nonNullable: true}
+        ),
+        email: new FormControl(
+          jsonSurveyQuestion?.questionValidators.email ?? false,
+          {nonNullable: true}
+        ),
+        pattern: new FormControl(
+          jsonSurveyQuestion?.questionValidators.pattern ?? ''
+        ),
+        nullValidator: new FormControl(
+          jsonSurveyQuestion?.questionValidators.nullValidator ?? false,
+          {nonNullable: true}
+        )
+      }),
+      questionChoices: this.createQuestionChoicesArray(jsonSurveyQuestion?.questionChoices),
+      questionControlOptions: new FormGroup<QuestionControlOptionsGroup>({
+        min: new FormControl(
+          jsonSurveyQuestion?.questionControlOptions?.min ?? 0,
+          {nonNullable: true, validators: [Validators.required]}
+        ),
+        max: new FormControl(
+          jsonSurveyQuestion?.questionControlOptions?.max ?? 10,
+          {nonNullable: true, validators: [Validators.required]}
+        ),
+        step: new FormControl(
+          jsonSurveyQuestion?.questionControlOptions?.step ?? 1,
+          {nonNullable: true, validators: [Validators.required]}
+        )
+      })
+    });
+
+    if (jsonSurveyQuestion?.questionType !== 'radio' && jsonSurveyQuestion?.questionType !== 'multiple-choice') {
+      newQuestion.controls.questionChoices.disable();
+    }
+
+    if (jsonSurveyQuestion?.questionType !== 'range') {
+      newQuestion.controls.questionControlOptions.disable();
+    }
+
+    return newQuestion;
+  }
+
+  private createQuestionChoicesArray(choices: string[] | undefined): FormArray<FormControl<string>> {
+    let formArray = new FormArray<FormControl<string>>([
+      new FormControl(
+        '',
+        {nonNullable: true, validators: [Validators.required]}
+      ),
+      new FormControl(
+        '',
+        {nonNullable: true, validators: [Validators.required]}
+      )
+    ], [Validators.minLength(2)]);
+
+    if (choices) {
+      formArray.clear();
+      choices.forEach(choice => formArray.push(new FormControl(
+        choice,
+        {nonNullable: true, validators: [Validators.required]}
+      )))
+    }
+
+    return formArray;
+  }
 }
