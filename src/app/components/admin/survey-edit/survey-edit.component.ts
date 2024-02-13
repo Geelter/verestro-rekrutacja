a9b67de7f65bb9db309b51fc7b945c814601e66f
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2024-02-13 04:15:25 +0100

    Create a wrapper component for form editor used on '/admin/surveys/edit'

---
+++ src/app/components/admin/survey-edit/survey-edit.component.ts
@@ -0,0 +1,38 @@
+import {Component, inject} from '@angular/core';
+import {FormEditorComponent} from "../form-editor/form-editor.component";
+import {AsyncPipe} from "@angular/common";
+import {SupabaseDatabaseService} from "../../../services/supabase/supabase-database.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {JsonSurveyData} from "../../../models/json-form-data.interface";
+import {catchError, map, switchMap, take, throwError} from "rxjs";
+
+@Component({
+  selector: 'app-survey-edit',
+  standalone: true,
+  imports: [
+    FormEditorComponent,
+    AsyncPipe
+  ],
+  templateUrl: './survey-edit.component.html',
+  styleUrl: './survey-edit.component.scss'
+})
+export class SurveyEditComponent {
+  private readonly databaseService = inject(SupabaseDatabaseService);
+  private readonly router = inject(Router);
+  private readonly activatedRoute = inject(ActivatedRoute);
+
+  readonly surveyData$ = this.activatedRoute.params.pipe(
+    map(params => +params['id']),
+    switchMap(surveyID => this.databaseService.fetchSurveyForID(surveyID)),
+    catchError(error => {
+      this.router.navigate(['/admin', 'surveys', 'list']);
+      return throwError(() => new Error(error))
+    })
+  )
+
+  surveyConfigSubmitted(surveyID: number, surveyConfig: JsonSurveyData) {
+    this.databaseService.updateSurvey(surveyID, surveyConfig).pipe(
+      take(1)
+    ).subscribe(() => this.router.navigate(['/admin', 'surveys', 'list']));
+  }
+}
