7abfeb6f99a79e17d941bfa93e435d925e01cb1b
Mateusz Jabłoniec <mateusz.s.jab@gmail.com>
2024-02-13 06:38:45 +0100

    Create a custom dialog for confirming deleting survey submissions

---
+++ src/app/components/delete-submission-dialog/delete-submission-dialog.component.ts
@@ -0,0 +1,34 @@
+import {Component, inject} from '@angular/core';
+import {MatButton} from "@angular/material/button";
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle
+} from "@angular/material/dialog";
+
+@Component({
+  selector: 'app-delete-submission-dialog',
+  standalone: true,
+    imports: [
+        MatButton,
+        MatDialogActions,
+        MatDialogClose,
+        MatDialogContent,
+        MatDialogTitle
+    ],
+  templateUrl: './delete-submission-dialog.component.html',
+  styleUrl: './delete-submission-dialog.component.scss'
+})
+export class DeleteSubmissionDialogComponent {
+  private readonly dialogRef = inject(MatDialogRef<DeleteSubmissionDialogComponent>);
+
+  reject() {
+    this.dialogRef.close(false);
+  }
+
+  accept() {
+    this.dialogRef.close(true);
+  }
+}
